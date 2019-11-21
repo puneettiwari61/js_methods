3a55c1414c92676a1f8b740b1a36e9b1fa561f57
Puneet <puneettiwari61@gmail.com>
2019-11-21 22:51:00 +0530

    finish

--- array-methods/1-array.js
+++ array-methods/1-array.js
@@ -5,46 +5,75 @@ var strings = ["this", "is", "a", "collection", "of", "words"];
 // Use the above two arrays and practice array methods
 
 // Find largest number in numbers
+console.log(Math.max(...numbers))
+
+
 
 // Find longest string in strings
 
+var newStrings = strings.sort((a,b) => b.length -  a.length)
+console.log(newStrings[0]);
+
 // Find all the even numbers
+
+var new1 = [];
+numbers.forEach((n) => (n%2==0) ? new1.push(n) : '');
+console.log(new1);
 
 // Find all the odd numbers
 
+var new2 = [];
+numbers.forEach((n) => (n%2 !=0 ) ? new2.push(n) : '');
+console.log(new2);
+
+
+
 // Find all the words that contain 'is' use string method 'includes'
+
+var inc;
+inc = strings.filter((s) => s.includes('is'));
+
 
 // Find all the words that contain 'is' use string method 'indexOf'
 
+var incs=[] ;
+strings.forEach((s) => s.indexOf('is') >=0? incs.push(s): '')
+
 // Check if all the numbers in numbers array are divisible by three use array method (every)
 
-//  Sort Array from smallest to largest
+numbers.every((n) => (n % 3 == 0)) ; 
 
+//  Sort Array from smallest to largest
+var ascend = []
+ascend = numbers.sort((a,b) => a-b);
 // Remove the last word in strings
 
+strings.pop()
+
 // Add a new word in the array
-
+strings.push('new Word')
 // Remove the first word in the array
-
+strings.shift();
 // Place a new word at the start of the array use (upshift)
-
+strings.unshift("new")
 // Make a subset of numbers array [18,9,7,11]
-
+var sub = numbers.splice(3,4);
 // Make a subset of strings array ['a','collection']
-
+var strsub = strings.splice(2,2);
 // Replace 12 & 18 with 1221 and 1881
-
+var newSub = strings
 // Replace words with string in strings array
-
+strings.splice(-1,1,"nowords")
 // Customers Array
 var customers = [
   { firstname: "Joe", lastname: "Blogs" },
   { firstname: "John", lastname: "Smith" },
-  { firstname: "Dave", lastname: "Jones" },
+  { firstname: "DJave", lastname: "Jones" },
   { firstname: "Jack", lastname: "White" }
 ];
 // Find all customers whose firstname starts with 'J'
-
+customers.filter((n) => n.firstname.indexOf('J') == 0? n.firstname: '')
 // Create new array with firstname and lastname
-
+var nc = customers.map((n) => n.firstname + " " + n.lastname);
 // Sort the array created above alphabetically
+nc.sort();
--- array-methods/4-array.js
+++ array-methods/4-array.js
@@ -29,6 +29,19 @@ var data = [
 // write a function that will sum all of the dogs ages in dog years using for loop.
 // 1 human year = 7 dog year
 
+var ds = 0;
+function dogAge(data){
+  for(var i =0; i<data.length ; i++){
+    if(data[i].type == 'dog'){
+    ds = ds + data[i].age
+  }
+}
+  return (7 * ds)
+}
+undefined
+dogAge(data)
+105
+
 // your code goes here
 
 // Solution is 105
@@ -39,3 +52,6 @@ var data = [
 // 3. reduce - to accumulate total age.
 
 // Solution 105
+var dogArr = data.filter((animal) => animal['type'] == 'dog' );
+var dogAge = dogArr.map((o) => o['age'] = 7 * o['age']);
+dogAge.reduce((a,b) => a+b);
